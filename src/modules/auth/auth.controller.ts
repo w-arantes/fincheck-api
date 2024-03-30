d626ecaf24918aa5e1c526be95c46e55837df813
Wellington Arantes <w.aranteslfc@gmail.com>
2024-03-29 23:02:15 -0300

    feat: add public routes decorator

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,10 +1,12 @@
 import { Body, Controller, Post } from '@nestjs/common';
 
+import { IsPublic } from 'src/shared/decorators/public-routes.decorator';
 import { AuthService } from './auth.service';
 import { SignInDTO } from './dtos/sign-in.dto';
 import { SignUpDTO } from './dtos/sign-up.dto';
 
 @Controller('auth')
+@IsPublic()
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
