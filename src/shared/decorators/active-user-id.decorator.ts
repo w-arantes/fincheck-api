41597729fa795085d43017581c755e9796576aa8
Wellington Arantes <w.aranteslfc@gmail.com>
2024-04-03 21:50:12 -0300

    feat: add categories module and categories repository

--- src/shared/decorators/active-user-id.decorator.ts
+++ src/shared/decorators/active-user-id.decorator.ts
@@ -5,7 +5,7 @@ import {
 } from '@nestjs/common';
 
 export const ActiveUserId = createParamDecorator<undefined>(
-  (data, context: ExecutionContext) => {
+  (_, context: ExecutionContext) => {
     const request = context.switchToHttp().getRequest();
     const userId = request.userId;
 
